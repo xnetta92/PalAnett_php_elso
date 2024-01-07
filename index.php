801af46ac35fe7b50a484c24cdcf6d0c4076e8d3
xnetta92 <palanett92@gmail.com>
2024-01-07 18:33:29 +0100

    navindex

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title>Spells</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
@@ -13,6 +13,21 @@
 </head>
 <body>
     <?php require_once "navigation.php" ?>
+    <main class="container mt-3">
+        <?php 
+        if (isset($_GET['page'])) {
+            $page_file = "pages/".$_GET["page"].".php";
+            if (file_exists($page_file)) {
+                require_once $page_file;
+            } else {
+                require_once "pages/404.php";
+            }
+        }
+        else {
+            require_once "pages/home.php";
+        }
+        ?>
+    </main>
 
 
 </body>
--- navigation.php
+++ navigation.php
@@ -1,17 +1,17 @@
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Spells</a>
+    <a class="navbar-brand" href="index.php">Spells</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="index.php?page=home">Home</a>
         </li>
-          <a class="nav-link" href="#">List</a>
+          <a class="nav-link" href="index.php?page=listspell">List</a>
           <li class="nav-item">
-          <a class="nav-link" href="#">Add</a>
+          <a class="nav-link" href="index.php?page=addspell">Add</a>
         </li>  
       </ul> 
     </div>
